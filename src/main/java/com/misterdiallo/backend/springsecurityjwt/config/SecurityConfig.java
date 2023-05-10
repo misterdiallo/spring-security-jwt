0d4292d63e58544acb5e2b15f1965a4d5fc10bcd
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2023-05-11 04:35:40 +0800

    Adding the Authorizations and permissions directly in the controller using annotations

--- src/main/java/com/misterdiallo/backend/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/com/misterdiallo/backend/springsecurityjwt/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ import static org.springframework.http.HttpMethod.*;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -50,13 +52,13 @@ public class SecurityConfig {
                 .requestMatchers(PUT, "/api/v1/manager/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
                 .requestMatchers(DELETE, "/api/v1/manager/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
 
-                // Securing the ADMIN endpoint with ADMIN ROLE only
+                /*// Securing the ADMIN endpoint with ADMIN ROLE only
                 .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
                 // Securing all the methods for the ADMIN endpoint with ADMIN PERMISSIONS only
                 .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
                 .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
                 .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
 
 
                 .anyRequest()
